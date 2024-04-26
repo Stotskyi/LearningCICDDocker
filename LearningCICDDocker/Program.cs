0ec8e851a4f78b541d18e901b1a527573522c57c
Stotskyi <andrii.stotskyi.u@gmail.com>
2024-04-26 18:03:07 +0300

    add cors

--- LearningCICDDocker/Program.cs
+++ LearningCICDDocker/Program.cs
@@ -1,13 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,7 +12,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(opts =>
+{
+    opts.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+});
+    
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
