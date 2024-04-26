73b28acdff9b6bf3c89c2aae0e5ecd8e7f5d7bd8
Stotskyi <andrii.stotskyi.u@gmail.com>
2024-04-26 18:00:00 +0300

    update client project

--- LearningCICDDockerClient/src/app/app.component.ts
+++ LearningCICDDockerClient/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'LearningCICDDockerClient';
+export class AppComponent implements OnInit {
+  stringResponse: string | undefined;
+
+  constructor(private apiService: ApiService) {}
+    
+  ngOnInit(): void {
+    this.fetchStringData();
+  }
+
+    fetchStringData(): void {
+      this.apiService.getData().subscribe(data => {
+        this.stringResponse = data;
+        console.log(this.stringResponse);
+      });
+  }
 }
